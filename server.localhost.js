054a1a997e99de5680e87e81f63df420d34a70f5
Owen Boyd <owen.boyd@D2L.com>
2024-02-15 09:35:42 -0500

    adding timestamp to server startup message to better understand when last reload occured

--- server.localhost.js
+++ server.localhost.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`${new Date()} Diagram Animator Web Server listening on port ${port}`);  
 });
